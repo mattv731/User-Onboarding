30ec720d708615c8b3e0ccd7f2d89d82177df912
abc <abc@example.com>
2021-11-10 19:34:08 -0700

    I started coding the form.js

--- user-onboarding/componants/form.js
+++ user-onboarding/componants/form.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
-function Form() {
+const Form = (props) => {
     return (
-        <div>
-            My Form
+        <div>     
+           <h1>
+        My Form
+           </h1>
+           <form>
+               <label>Name
+                <input 
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={onChange}
+                />
+               </label>
+               <label>Email
+                   <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                    />
+               </label>
+               <label>Password
+                   <input
+                   type="password"
+                   name="password"
+                   value={password}
+                   onChange={onChange}
+                   />
+               </label>
+               <label></label>
+           </form>
         </div>
     )
 }
+export default Form;
